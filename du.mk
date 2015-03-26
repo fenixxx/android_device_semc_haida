1d4bf351d2c7ae222171d8f46e54140ffad2a823
fenix <vickychauhan111@gmail.com>
2015-03-26 07:34:46 +0100

    DU-ify     git commit --amend --reset-author Change-Id: Icbaf00830b300a41024ef660b77ae7a42c9ff934
        renamed: cm.mk -> du.mk
        modified: vendorsetup.sh
    
    Change-Id: Ie7b98d845d8f15d9ca9c86d55b969c2053b2682f

--- du.mk
+++ du.mk
@@ -16,7 +16,7 @@
 $(call inherit-product, device/semc/haida/full_haida.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += \
@@ -26,7 +26,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="MT11i-user 4.0.4 4.1.B.0.587 tL1_3w test-keys"
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_haida
+PRODUCT_NAME := du_haida
 PRODUCT_GMS_CLIENTID_BASE := android-sonyericsson
 
 # Custom tag for unofficial builds
